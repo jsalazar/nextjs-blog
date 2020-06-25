ac47fb44748fdf2f95d18f80c1edccf18a3080de
Jake Salazar <jake.salazar@gmail.com>
2020-06-25 00:35:07 -0400

    fixed homepage typo

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,7 @@ export default function Home({allPostsData}) {
       <section className={utilStyles.headingMd}>
         <p>Just another person wondering why I'm here...</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          I love learning!!!
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
